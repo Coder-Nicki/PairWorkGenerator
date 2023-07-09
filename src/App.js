72f409d324fd310f3c1c3643e78d304c2b2ec440
Coder-Nicki <nicki5389@gmail.com>
2023-07-09 15:38:37 +1000

    Made random name generator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+const StudentPairing = ({ students }) => {
+  const [pairs, setPairs] = useState([]);
+
+  const handlePairing = () => {
+    const shuffledStudents = [...students].sort(() => Math.random() - 0.5);
+    const pairedStudents = [];
+
+    for (let i = 0; i < shuffledStudents.length; i += 2) {
+      if (i + 1 < shuffledStudents.length) {
+        pairedStudents.push([shuffledStudents[i], shuffledStudents[i + 1]]);
+      } else {
+        pairedStudents.push([shuffledStudents[i]]);
+      }
+    }
+
+    setPairs(pairedStudents);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button className="pair-button" onClick={handlePairing}>
+        Pair Students
+      </button>
+      {pairs.map((pair, index) => (
+        <div key={index} className="pair-text">
+          Pair {index + 1}: {pair.join(' and ')}
+        </div>
+      ))}
     </div>
   );
-}
+};
+
+const App = () => {
+  const students = ['Grace', 'Halle', 'Rupert', "Hudson", 'Ari', 'Brylie', 'Sarah', 'Cooper', 'Jayden', 'Austin', 'Adelin'];
+
+  return (
+    <div className="container">
+      <h1 className="title">Student Pairing</h1>
+      <StudentPairing students={students} />
+    </div>
+  );
+};
 
 export default App;
